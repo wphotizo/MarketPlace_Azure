af0b9591c70a439c03371c75db43cbde71bda680
Photizo <Photizo@DESKTOP-51F18D6>
2020-10-21 08:19:09 -0700

    Add project files.

---
+++ MarketPlace/Controllers/LoginController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MarketPlace_DAL;
+using MarketPlace_Services;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace MarketPlace.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        //Login Action
+        public ActionResult Authenticate(tblUser user)
+        {
+
+            if (ModelState.IsValid)
+            {
+                Service loginService = new Service();
+                var IsValid = loginService.LoginCustomer(user.email, user.password);
+                
+                if (IsValid.ToString() == "True")
+                {
+                    ViewBag.message = "";
+                }
+                else
+                {
+                    ViewBag.message = "Incorrect username or password";
+                    return View("Login");
+                }
+             
+                return IsValid ? RedirectToAction("SearchPage", "Search") :  RedirectToAction("Login", "Login");
+            }
+            else
+            {
+                
+                return View("Login");
+            }
+
+
+        }
+
+        // Redundant Save New User
+        public ActionResult Insert(tblUser user)
+        {
+            Service insert_Service = new Service();
+            insert_Service.saveCustomer(user);
+            return View("Login");
+        }
+
+        [HttpPost]
+        public ActionResult Save(FormCollection formCollection, HttpPostedFileBase file)
+        {
+            tblUser user = new tblUser();
+      
+         
+
+         
+            if (file != null)
+            {
+          
+                string ImageName = System.IO.Path.GetFileName(file.FileName);
+                string physicalPath = Server.MapPath("~/Images/" + ImageName);
+
+                //save image in folder
+                file.SaveAs(physicalPath);
+
+                user.username = formCollection["username"];
+                user.email = formCollection["email"];
+                user.password = formCollection["password"];
+                user.picture = ImageName;
+
+              
+
+                Service insert_Service = new Service();
+                insert_Service.saveCustomer(user);
+
+            }
+
+            return Content("User successfully saved");
+        }
+    }
+
+     
+ }
